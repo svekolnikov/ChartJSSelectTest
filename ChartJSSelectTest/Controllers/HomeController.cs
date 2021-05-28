8e855dd5c5433dc6c861dabc2764f4fce7e71817
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-05-29 02:57:15 +0300

    Add project files.

---
+++ ChartJSSelectTest/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ChartJSSelectTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ChartJSSelectTest.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SaveData([FromBody] List<SelectedDataModel> models)
+        {
+            var date = models?[0].x.ToLocalTime();
+            return Json($"First date: {date}");
+        }
+    }
+}
---
+++ ChartJSSelectTest/Models/SelectedDataModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ChartJSSelectTest.Models
+{
+    public class SelectedDataModel
+    {
+        public DateTime x { get; set; }
+        public double y { get; set; }
+    }
+}
