b5096f16494b2a013b5981e7217662b3d60abf2b
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-05-29 14:19:49 +0300

    save chart as jpeg or png to server

--- ChartJSSelectTest/Controllers/HomeController.cs
+++ ChartJSSelectTest/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using ChartJSSelectTest.Models;
+using ChartJSSelectTest.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace ChartJSSelectTest.Controllers
 {
@@ -18,6 +20,29 @@ namespace ChartJSSelectTest.Controllers
         {
             var date = models?[0].x.ToLocalTime();
             return Json($"First date: {date}");
+        }
+
+        [HttpPost]
+        public JsonResult SaveImg([FromBody] string img)
+        {
+            Bitmap chartBitmap = img.Base64StringToBitmap();
+
+            // Assumes myImage is the PNG you are converting
+            using (var b = new Bitmap(chartBitmap))
+            {
+                b.SetResolution(chartBitmap.HorizontalResolution, chartBitmap.VerticalResolution);
+
+                using (var g = Graphics.FromImage(b))
+                {
+                    g.Clear(Color.White);
+                    g.DrawImageUnscaled(chartBitmap, 0, 0);
+                }
+
+                b.Save("chartBitmap.jpeg", ImageFormat.Jpeg);
+            }
+
+            chartBitmap.Save("chartBitmap.png", ImageFormat.Png);
+            return Json("Image saved");
         }
     }
 }
---
+++ ChartJSSelectTest/Services/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace ChartJSSelectTest.Services
+{
+    public static class Converter
+    {
+        public static Bitmap Base64StringToBitmap(this string base64String)
+        {
+            Bitmap bmpReturn = null;
+
+            var splitComma = base64String.Split(','); // delete header
+
+            byte[] byteBuffer = Convert.FromBase64String(splitComma[1]);
+            MemoryStream memoryStream = new MemoryStream(byteBuffer);
+
+            memoryStream.Position = 0;
+
+            bmpReturn = (Bitmap)Bitmap.FromStream(memoryStream);
+
+            memoryStream.Close();
+
+            return bmpReturn;
+        }
+    }
+}
